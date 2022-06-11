c623513e195c1d2b0878080f48cfbe6f81ed379d
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-12 00:21:04 +0300

    Created users projects routes in urls file

---
+++ projects/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+   
+    path('uploads/', views.uploads, name='uploads'),
+    path('uploads/upload', views.upload, name='upload'),
+    path('post/<str:pk>/',views.view_post, name='viewpost'),
+    
+    
+]
