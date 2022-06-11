05f638020ed97877fe306dc044b60bff64d69f7f
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-11 21:11:41 +0300

    Added profile setting route and profile route

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.landing, name='landing'),   
+    path('', views.landing, name='landing'),  
+    path('signup/', views.signup, name='signup'),
+    path('settings/', views.settings, name='settings'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout, name='logout'),
+     path('profile/<str:pk>', views.profile, name='profile') 
 ]
