3287b88662761d0b31d54790a6011f515675b16d
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-12 15:35:49 +0300

    Added url path for projects page

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('uploads/', views.uploads, name='uploads'),
     path('uploads/upload', views.upload, name='upload'),
     path('post/<str:pk>/',views.view_post, name='viewpost'),
+    path('projects/', views.projects, name='projects'),
     
     
 ]
