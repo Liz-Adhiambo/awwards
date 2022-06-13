188e5877d5f65e106f8d11917b610b08020bbbd8
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-13 15:55:33 +0300

    Added ratings route to urls.py file

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('uploads/upload', views.upload, name='upload'),
     path('post/<str:pk>/',views.view_post, name='viewpost'),
     path('projects/', views.projects, name='projects'),
+    path('<pk>/rate', views.Rate, name='rate-project'),
+    path('search', views.search, name='search'),
     
     
 ]
