0f6a7b84712fec9b6c7717bde814cfdfbf2d41ff
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-13 15:54:43 +0300

    Added rating function to capture rating form details and display user ratings

--- projects/views.py
+++ projects/views.py
@@ -1,22 +1,28 @@
 import random
 from django.core.exceptions import ObjectDoesNotExist
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 from django.shortcuts import get_object_or_404, render,redirect
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from itertools import chain
-
+from projects.forms import RatingForm
+from django.template import loader
+from django.db.models import Avg
 from users.models import Profile
-from.models import  Project
+from.models import  Project, Rating
+from itertools import chain
 # Create your views here.
 
 
+
+
+@login_required(login_url='signin')
 def uploads(request):
     return render(request,'uploads.html')
 
+@login_required(login_url='signin')
 def upload(request):
 
     if request.method == 'POST':
@@ -34,10 +40,17 @@ def upload(request):
     else:
         return redirect('/')
 
+@login_required(login_url='signin')
 def view_post(request,pk):
     # post_id = request.GET.get('id')
     project = Project.objects.get(id=pk)
-    
+
+    ratings = Rating.objects.filter(project=project)
+    reviews_avg = ratings.aggregate(Avg('design'))
+    ureviews_avg=ratings.aggregate(Avg('usability'))
+    creviews_avg=ratings.aggregate(Avg('content'))
+
+    reviews_count = ratings.count()
     
     # try:
     #     # post = Post.objects.get(id=post_id)
@@ -50,10 +63,17 @@ def view_post(request,pk):
     
     context = {
         'project':project,
-        
+        'reviews': ratings,
+		'reviews_avg': reviews_avg,
+		
+        'ureviews_avg':ureviews_avg,
+        'creviews_avg':creviews_avg,
+        'reviews_count': reviews_count
         }   
     return render(request,'view_project.html',context)
 
+  
+@login_required(login_url='signin')
 def projects(request):
     
     
@@ -73,4 +93,56 @@ def projects(request):
     
     
 
-    return render(request,'projects.html' ,{"images":images,})
+    return render(request,'projects.html' ,{"images":images})
+
+
+def Rate(request, pk):
+	project = Project.objects.get(id=pk)
+	user = request.user
+
+    
+    
+
+
+	if request.method == 'POST':
+		form = RatingForm(request.POST)
+		if form.is_valid():
+			rate = form.save(commit=False)
+			rate.user = user
+			rate.project = project
+			rate.save()
+			return HttpResponseRedirect(reverse('viewpost', args=[pk]))
+	else:
+		form = RatingForm()
+
+	template = loader.get_template('rate.html')
+
+	context = {
+		'form': form, 
+		'project': project,
+	}
+
+	return HttpResponse(template.render(context, request))
+
+
+@login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+        
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
