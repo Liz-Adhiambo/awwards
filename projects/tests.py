f70769ee176ce615683548a26fb5d6c897be6837
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-14 22:33:27 +0300

    Added the [rofile testclass in the users app and ran tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from users.models import Profile
+
 from .models import  Project, Rating
 # Create your tests here.
 
@@ -30,4 +32,34 @@ class ProjectTestClass(TestCase):
         self.assertTrue(project.username == 'galleria')
  
  
- #test for Post Model
+ #test for Rating Model
+class RatingsTestClass(TestCase):
+  def setUp(self):
+    self.user = Profile.objects.create(username='Liz')
+    self.project=Project.objects.create(id=1,tittle='glowing-stars',description='just neon',project_image='image.png',urls='www.neon.com',profile=self.user)
+    self.rating = Rating.objects.create(id=1, design=9, usability=10, content=10, profile=self.user, prjects=self.project)
+
+  def tearDown(self):
+    Profile.objects.all().delete()
+    Project.objects.all().delete()
+    Rating.objects.all().delete()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.rating,Rating))
+  
+  def test_save_rating(self):
+    self.rating = Rating.objects.create(id=1, design=9, usability=10, content=10, profile=self.user, projects=self.project)
+    self.rating.save_rating()
+    ratings=Rating.objects.all()
+    self.assertEqual(len(ratings),1)
+  
+  def test_delete_rating(self):
+    self.rating = Rating.objects.create(id=1, design=9, usability=10, content=10, profile=self.user, prjects=self.project)
+    self.rating.save_rating()
+    ratings = Rating.objects.all()
+    self.assertEqual(len(ratings), 1)
+    self.rating.delete_rating()
+    rate=Rating.objects.all()
+    self.assertEqual(len(rate),0)
+
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.test import TestCase
 
+from .models import   Profile
+# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.prof = Profile(name="Liz")
+        self.prof.save_profile()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.prof,Profile))
+
+    def test_save_method(self):
+        self.prof.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.prof.save_profile()
+        self.prof.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) == 0)
+
+    def test_update(self):
+        profile = Profile.get_profile_id(self.prof.id)
+        profile.update_profile('liz')
+        profile = Profile.get_profile_id(self.prof.id)
+        self.assertTrue(profile.username == 'liz')
+ 
+ 
+ 
 # Create your tests here.
