b64e8b30901b1855eb6c139b40e2d332176a7456
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-14 10:32:18 +0300

    Added the api profile path to users urls file

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.landing, name='landing'),  
     path('signup/', views.signup, name='signup'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('logout/', views.logout, name='logout'),
     path('profile/<str:pk>', views.profile, name='profile'), 
+    path('api/profile/', views.ProfileList.as_view()),
     
 ]
